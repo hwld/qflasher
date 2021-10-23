4a3a93a337c5a5ee76d11cd7b77f071302fbf5d5
hwld <0701hw@gmail.com>
2021-10-23 21:44:25 +0900

    Change to end loading when unmounting.

--- hooks/useLoadingEffect.tsx
+++ hooks/useLoadingEffect.tsx
@@ -16,4 +16,15 @@ export const useLoadingEffect = (isLoading: boolean) => {
       loadingId.current = undefined;
     }
   }, [endLoading, isLoading]);
+
+  // 破棄されたらローディングを終了させる
+  useEffect(() => {
+    return () => {
+      if (loadingId.current) {
+        endLoading(loadingId.current);
+        loadingId.current = undefined;
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
