56b53203d4dc530dcba36950206f4ac0d4a19304
hwld <0701hw@gmail.com>
2021-11-11 23:25:33 +0900

    Refactor

--- components/DeckPlayer/index.tsx
+++ components/DeckPlayer/index.tsx
@@ -1,11 +1,10 @@
 import { Flex, FlexProps } from "@chakra-ui/react";
-import React, { Reducer, useReducer } from "react";
-import { Deck, FlashCard } from "../../types";
-import { assertNever } from "../../utils/assertNever";
-import { shuffle } from "../../utils/shuffle";
+import React from "react";
+import { Deck } from "../../types";
 import { DeckPlayConfig } from "../pages/DeckPlayerPage";
 import { FlashCardViewer } from "./FlashCardViewer";
 import { OperationBar } from "./OperationBar";
+import { useDeckPlayerState } from "./useDeckPlayerState";
 
 type Props = {
   deck: Deck;
@@ -13,130 +12,25 @@ type Props = {
   size: "sm" | "md";
 } & FlexProps;
 
-type State = {
-  initialCards: FlashCard[];
-  config: DeckPlayConfig;
-  cardStack: FlashCard[];
-  rightAnswerCount: number;
-  wrongCards: FlashCard[];
-  totalCardsCount: number;
-  progress: number;
-  front: "question" | "answer";
-};
-type Action = "trunOver" | "right" | "wrong" | "replayAll" | "replayWrong";
-
-const reducer: Reducer<State, Action> = (state, action) => {
-  const {
-    initialCards,
-    config,
-    cardStack,
-    rightAnswerCount,
-    wrongCards,
-    totalCardsCount,
-    front,
-  } = state;
-
-  switch (action) {
-    case "trunOver": {
-      return {
-        ...state,
-        front: front === "question" ? "answer" : "question",
-      };
-    }
-    case "right": {
-      return {
-        ...state,
-        rightAnswerCount: rightAnswerCount + 1,
-        cardStack: cardStack.slice(0, -1),
-        progress:
-          ((totalCardsCount - (cardStack.length - 1)) / totalCardsCount) * 100,
-        front: config.initialFront,
-      };
-    }
-    case "wrong": {
-      const wrongCard = cardStack[cardStack.length - 1];
-      if (!wrongCard) {
-        throw new Error("存在しないカード");
-      }
-
-      return {
-        ...state,
-        wrongCards: [...wrongCards, wrongCard],
-        cardStack: cardStack.slice(0, -1),
-        progress:
-          ((totalCardsCount - (cardStack.length - 1)) / totalCardsCount) * 100,
-        front: config.initialFront,
-      };
-    }
-    case "replayAll": {
-      return {
-        ...state,
-        cardStack: buildCardStack(initialCards, config.isOrderRandom),
-        rightAnswerCount: 0,
-        wrongCards: [],
-        totalCardsCount: initialCards.length,
-        progress: 0,
-        front: config.initialFront,
-      };
-    }
-    case "replayWrong": {
-      return {
-        ...state,
-        cardStack: buildCardStack(wrongCards, config.isOrderRandom),
-        rightAnswerCount: 0,
-        wrongCards: [],
-        totalCardsCount: wrongCards.length,
-        progress: 0,
-        front: config.initialFront,
-      };
-    }
-    default: {
-      assertNever(action);
-    }
-  }
-};
-
-const buildCardStack = (cards: FlashCard[], isOrderRandom: boolean) => {
-  const stack = isOrderRandom ? shuffle(cards) : cards;
-  return [...stack].reverse();
-};
-
 export const DeckPlayer: React.FC<Props> = ({
   deck,
   config,
   size,
   ...styles
 }) => {
-  const [state, dispatch] = useReducer(reducer, {
-    initialCards: deck.cards,
-    config,
-    cardStack: buildCardStack(deck.cards, config.isOrderRandom),
-    rightAnswerCount: 0,
-    wrongCards: [],
-    totalCardsCount: deck.cards.length,
-    progress: 0,
-    front: config.initialFront,
-  });
-
-  const handleTurnOver = () => {
-    dispatch("trunOver");
-  };
-
-  const handleRight = () => {
-    dispatch("right");
-  };
-
-  const handleWrong = () => {
-    dispatch("wrong");
-  };
-
-  const handleReplayAll = () => {
-    dispatch("replayAll");
-  };
-
-  const handleReplayWrong = () => {
-    dispatch("replayWrong");
-  };
+  const {
+    cardStack,
+    rightAnswerCount,
+    wrongCards,
+    totalCardsCount,
+    progress,
+    front,
+    handleTurnOver,
+    handleRight,
+    handleWrong,
+    handleReplayAll,
+    handleReplayWrong,
+  } = useDeckPlayerState(deck, config);
 
   return (
     <Flex direction="column" {...styles}>
@@ -147,17 +41,17 @@ export const DeckPlayer: React.FC<Props> = ({
         flexGrow={1}
         size={size}
         initialFront={config.initialFront}
-        totalCardsCount={state.totalCardsCount}
-        rightAnswersCount={state.rightAnswerCount}
-        cards={state.cardStack}
-        progress={state.progress}
-        topFront={state.front}
+        totalCardsCount={totalCardsCount}
+        rightAnswersCount={rightAnswerCount}
+        cards={cardStack}
+        progress={progress}
+        topFront={front}
       />
       <OperationBar
         my={5}
         size={size}
-        isEnd={state.cardStack.length === 0}
-        wrongAnswerCount={state.wrongCards.length}
+        isEnd={cardStack.length === 0}
+        wrongAnswerCount={wrongCards.length}
         onTurnOver={handleTurnOver}
         onRight={handleRight}
         onWrong={handleWrong}
