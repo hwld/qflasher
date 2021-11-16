6ea973ee338b3b66bd88e545e6f9e5930d7506ed
hwld <0701hw@gmail.com>
2021-11-16 23:43:55 +0900

    Change UI

--- components/pages/NotFoundDeckPage.tsx
+++ components/pages/NotFoundDeckPage.tsx
@@ -1,10 +1,33 @@
-import { Center, Heading } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { useRouter } from "next/dist/client/router";
 import React from "react";
 
 export const NotFoundDeckPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.push("/");
+  };
+
   return (
-    <Center mt={5}>
-      <Heading>存在しないデッキです</Heading>
-    </Center>
+    <Box flexGrow={1}>
+      <Flex
+        direction="column"
+        justify="space-around"
+        align="center"
+        mx="auto"
+        mt={10}
+        bgColor="gray.700"
+        w="800px"
+        h="25%"
+        rounded="lg"
+      >
+        <Heading>デッキが存在しません</Heading>
+        <Button colorScheme="green" onClick={handleBack}>
+          ホーム画面に戻る
+        </Button>
+      </Flex>
+    </Box>
   );
 };
