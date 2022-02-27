5fd60b5eb58ff90341534a52d1ac095a831df550
hwld <0701hw@gmail.com>
2022-02-27 23:19:41 +0900

    Fix flickering in image

--- src/components/ui/AppLogo.tsx
+++ src/components/ui/AppLogo.tsx
@@ -1,8 +1,25 @@
 import { Image, ImageProps } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = ImageProps;
 
 export const AppLogo: React.FC<Props> = ({ ...styles }) => {
-  return <Image alt="logo" src="/logo.svg" {...styles} />;
+  const [isLoadingFinished, setIsLoadingFinished] = useState(false);
+
+  const handleLoad = () => {
+    setIsLoadingFinished(true);
+  };
+
+  return (
+    <Image
+      alt="logo"
+      // 読込中に一瞬だけ代替テキストや画像破損のアイコンが表示されてしまうので
+      // 読み込みが完了、または読み込みエラーが発生するまでは要素を表示しない
+      opacity={isLoadingFinished ? 1 : 0}
+      src="/logo.svg"
+      onLoad={handleLoad}
+      onError={handleLoad}
+      {...styles}
+    />
+  );
 };
