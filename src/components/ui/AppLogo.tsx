823750674beab0db577e90c2b41706027267b403
hwld <0701hw@gmail.com>
2022-03-09 01:10:44 +0900

    Change UI

--- src/components/ui/AppLogo.tsx
+++ src/components/ui/AppLogo.tsx
@@ -4,5 +4,13 @@ import React from "react";
 type Props = ImageProps;
 
 export const AppLogo: React.FC<Props> = ({ ...styles }) => {
-  return <Image alt="logo" fallback={<></>} src="/logo.svg" {...styles} />;
+  return (
+    <Image
+      alt="logo"
+      fallback={<></>}
+      src="/logo.svg"
+      userSelect={"none"}
+      {...styles}
+    />
+  );
 };
--- src/components/ui/SignInForm/SignInForm.tsx
+++ src/components/ui/SignInForm/SignInForm.tsx
@@ -1,8 +1,9 @@
+import { AppLogo } from "@/components/ui/AppLogo";
 import { GoogleSignInButton } from "@/components/ui/SignInForm";
 import { AnonymousSignInButton } from "@/components/ui/SignInForm/AnonymousSignInButton";
 import { useAppOperation } from "@/hooks/useAppOperation";
 import { useAuthState } from "@/hooks/useAuthState";
-import { Box, BoxProps } from "@chakra-ui/react";
+import { Box, BoxProps, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 type Props = BoxProps;
@@ -14,9 +15,42 @@ export const SignInForm: React.FC<Props> = ({ children, ...styles }) => {
   const anonymousSignIn = useAppOperation(signInAnonymous);
 
   return (
-    <Box bgColor="gray.700" padding={10} boxShadow="dark-lg" {...styles}>
-      <GoogleSignInButton onClick={googleSignIn} />
-      <AnonymousSignInButton mt={3} onClick={anonymousSignIn} />
+    <Box
+      w="90%"
+      maxW="400px"
+      h="400px"
+      bgColor="gray.700"
+      boxShadow="dark-lg"
+      rounded={"md"}
+      overflow="hidden"
+      display="flex"
+      flexDir={"column"}
+      {...styles}
+    >
+      <Flex
+        bgGradient={"linear(to-br, green.400 75%, orange.300 75%)"}
+        flexGrow={1}
+        justify="center"
+        align={"center"}
+      >
+        <AppLogo width={{ base: "70%", md: "80%" }} />
+      </Flex>
+      <Flex mx="auto" pos={"relative"} flexDir="column" align={"center"}>
+        <Text
+          pos={"absolute"}
+          top={"-20px"}
+          fontSize="xl"
+          fontWeight={"bold"}
+          px={5}
+          py={2}
+          backgroundColor="gray.700"
+          rounded={"xl"}
+        >
+          Sign in
+        </Text>
+        <GoogleSignInButton onClick={googleSignIn} mt={14} />
+        <AnonymousSignInButton mt={5} onClick={anonymousSignIn} mb={14} />
+      </Flex>
     </Box>
   );
 };
