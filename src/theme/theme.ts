9e516cd4ce1a22ac09f5219fc05b107d05ecfbed
hwld <0701hw@gmail.com>
2022-05-26 23:23:47 +0900

    Improve flickering of box-shadow in MenuList

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -10,7 +10,21 @@ export const theme = extendTheme({
     useSystemColorMode: false,
   },
   styles: {
-    global: {},
+    global: {
+      // focus-visibleパッケージを入れて、chakra-uiのクリック時のフォーカスリングを消そうとすると、
+      // focusがあたってる要素に強制的にbox-shadow:noneが与えられ、box-shadowの設定がリセットされてしまう。
+      // menuなどのコンポーネントは、フォーカスがあたってる状態でbox-shadowが設定されているため、その設定が上書きされる。
+      // なので、以下の一時的な回避策を使う。
+      // https://github.com/chakra-ui/chakra-ui/issues/4276#issuecomment-1096055757
+
+      // https://github.com/chakra-ui/chakra-ui/blame/790d2417a3f5d59e2d69229a027af671c2dc0cbc/packages/css-reset/src/css-reset.tsx#L274
+      // に書いてあるとおり、現時点での最新のcss-resetでは、not([data-focus-visible-disabled])というものが追加されており、
+      // data-focus-visible-disabledを渡すことによってこれをなくすことができるかもしれないが、そのバージョンはReact18を前提とするので、
+      // ひとまずは以下のコードで我慢する。
+      ".chakra-menu__menu-list:focus:not([data-focus-visible-added])": {
+        boxShadow: "var(--chakra-shadows-dark-lg)",
+      },
+    },
   },
   colors: {
     gray: {
