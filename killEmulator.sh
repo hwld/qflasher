31539f9f7916ea95b9a069fad3a820e7c229b9cd
hwld <0701hw@gmail.com>
2022-06-10 08:57:21 +0900

    Change killEmulator.sh

--- killEmulator.sh
+++ killEmulator.sh
@@ -1,3 +1,3 @@
 # firebaeのエミュレータが生きてることがあるのでシャットダウンする
 # windowsでgit bashを使ってる
-netstat -aon | grep -E "(4000|8080|9099|5000).+LISTENING" | awk '{print "//pid " $5}' | xargs taskkill //f
+netstat -aon | grep -E "(4000|8080|9099|5000).+LISTENING" | awk '{print  $5}' | sort | uniq | xargs -I {} taskkill //f //pid {}
